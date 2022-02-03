8103dbd93f54308f883a2cac90c716a5a90deee3
cheesycod <cheesycod@outlook.com>
2022-02-03 09:14:52 +0000

    update manager

--- commands.py
+++ commands.py
@@ -2,14 +2,13 @@ from hashlib import sha512
 import hmac
 import time
 import aiohttp
-from fateslist.classes import UserState, Status
-from fateslist import UserClient, APIResponse
+from fateslist.classes import UserState, BotState, Status, LongDescType
+from fateslist import BotClient, UserClient, APIResponse
 from fateslist.utils import etrace 
 from fateslist.system import SystemClient
-from discord import Color, Embed, User
+from discord import Color, Embed, User, ButtonStyle
+from discord.ui import Button, View
 from discord.ext import commands, tasks
-
-import json
 
 class Users(commands.Cog):
     """Commands made specifically for users to use"""
@@ -21,9 +20,9 @@ class Users(commands.Cog):
 
     @commands.command(
         name="catid",
-        description="Get the category ID of a channel",
     )
     async def catid(self, inter):
+        """Get the category ID of a channel"""
         return await self._catid(inter)
     
     @commands.slash_command(
@@ -35,9 +34,9 @@ class Users(commands.Cog):
 
     @commands.command(
         name="vote",
-        description="Vote for a bot"
     )
     async def _vote_normal(self, ctx, bot: User):
+        """Vote for a bot"""
         await self._vote_(ctx, bot)
     
     async def _vote_(self, inter, bot: User):
@@ -81,24 +80,40 @@ class Users(commands.Cog):
                     return await inter.send(f'{json["reason"]}\n**Status Code:** {res.status}')
                 return await inter.send("Successfully voted for this bot!")
 
-    @commands.command(name="chanid",
-                            description="Get channel id")
+    @commands.command(name="chanid")
     async def chanid(self, inter):
+        """Get the channel ID of the current channel"""
         return await inter.send(str(inter.channel.id))
 
-    @commands.command(name="flstats",
-                            description="Show Fates List Stats")
+    @commands.command(name="stats")
     async def stats(self, inter):
+        """Show Fates List statistics"""
         sc = SystemClient()
         stats = await sc.blstats()
         return await inter.send(embed=stats.embed())
 
+    @commands.command(name="profile")
+    async def _flprofile_normal(self, inter, user: User = None):
+        """Show a user's Fates List profile"""
+        if user is None:
+            user = inter.author
+        return await self._profile(inter, user)
+
     @commands.slash_command(
-        name="flprofile",
+        name="profile",
         description="Get your own or another users profile",
     )
     async def flprofile(self, inter, user: User = None):
+        if user is None:
+            user = inter.author
         return await self._profile(inter, user)
+    
+    @commands.command(name="bot")
+    async def _bot_normal(self, inter, user: User):
+        """Get a user's bot profile"""
+        if not user.bot:
+            return await inter.send("You can only get a bot like this!")
+        return await self._bot(inter, user)
 
     @tasks.loop(seconds=60)
     async def statloop(self):
@@ -129,38 +144,110 @@ class Users(commands.Cog):
         return await inter.send("No category attached to this channel")
 
     @staticmethod
-    async def _profile(inter, user=None):
-        """Gets a users profile (Not yet done)"""
-        target = user if user else inter.author
+    async def _bot(inter, user: User):
+        """Returns a bot"""
+        bc = BotClient(user.id)
+        _bot = await bc.get_bot(compact=False)
+        if isinstance(_bot, APIResponse):
+            return await inter.send("Failed to get bot!")
+        embed = Embed(
+            title=f"{user}'s Info", 
+            color=Color.blue(),
+            description="Here is your profile")
+        
+        # Base fields
+        embed.add_field(name="User ID", value=_bot.user["id"])
+        embed.add_field(name="Username", value=_bot.user["username"])
+        embed.add_field(name="Discriminator/Tag",
+                        value=_bot.user["disc"])
+        embed.add_field(name="Avatar", value=_bot.user["avatar"])
+        embed.add_field(name="Description",
+                        value=_bot.description)
+        embed.add_field(name="Long Description",
+                        value=_bot.long_description[:128]+"...")
+        embed.add_field(name="Long Description Type",
+                        value=f"{_bot.long_description_type} ({LongDescType(_bot.long_description_type).__doc__})")
+        embed.add_field(name="State",
+                        value=f"{_bot.state} ({BotState(_bot.state).__doc__})")
+        embed.add_field(name="Tags",
+                        value=", ".join(_bot.tags))
+
+
+        class BotButtonView(View):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.add_item(Button(url=f"https://fateslist.xyz/bot/{_bot.user['id']}", label="View"))
+                self.add_item(Button(url=f"https://fateslist.xyz/bot/{_bot.user['id']}/vote", label="Vote"))    
+                self.add_item(Button(url=f"https://fateslist.xyz/bot/{_bot.user['id']}/settings", label="Settings"))    
+
+
+        return await inter.send(
+            embed=embed, 
+            view=BotButtonView())
+
+    @staticmethod
+    async def _profile(inter, target: User):
+        """Gets a users profile"""
         uc = UserClient(target.id)
         _profile = await uc.get_user()
         if isinstance(_profile, APIResponse):
-            return
+            return await inter.send("Failed to get profile!")
         embed = Embed(title=f"{target}'s Profile",
                       description="Here is your profile")
 
-        _profile = _profile.dict()
-
         # Base fields
-        embed.add_field(name="User ID", value=_profile["user"]["id"])
-        embed.add_field(name="Username", value=_profile["user"]["username"])
+        embed.add_field(name="User ID", value=_profile.user["id"])
+        embed.add_field(name="Username", value=_profile.user["username"])
         embed.add_field(name="Discriminator/Tag",
-                        value=_profile["user"]["disc"])
-        embed.add_field(name="Avatar", value=_profile["user"]["avatar"])
+                        value=_profile.user["disc"])
+        embed.add_field(name="Avatar", value=_profile.user["avatar"])
         embed.add_field(name="Description",
-                        value=_profile["profile"]["description"])
+                        value=_profile.profile["description"])
         embed.add_field(
             name="Status",
-            value=f"{_profile['user']['status']} ({Status(_profile['user']['status']).__doc__})",
+            value=f"{_profile.user['status']} ({Status(_profile.user['status']).__doc__})",
         )
         embed.add_field(
             name="State",
-            value=f"{_profile['profile']['state']} ({UserState(_profile['profile']['state']).__doc__})",
+            value=f"{_profile.profile['state']} ({UserState(_profile.profile['state']).__doc__})",
         )
         embed.add_field(
             name="User CSS",
-            value=_profile["profile"]["user_css"]
-            if _profile["profile"]["user_css"] else "No custom user CSS set",
+            value=_profile.profile["user_css"]
+            if _profile.profile["user_css"] else "No custom user CSS set",
         )
 
         await inter.send(embed=embed)
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            return
+        if isinstance(error, commands.MissingRequiredArgument):
+            return await ctx.send(
+                f"You are missing a required argument: {error.param}!"
+            )
+        if isinstance(error, commands.BadArgument):
+            return await ctx.send(
+                f"You have provided an invalid argument: {error}!"
+            )
+        if isinstance(error, commands.CommandOnCooldown):
+            return await ctx.send(
+                f"You are on cooldown!"
+            )
+        if isinstance(error, commands.MissingPermissions):
+            return await ctx.send(
+                f"You are missing permissions!"
+            )
+        if isinstance(error, commands.CommandInvokeError):
+            return await ctx.send(
+                f"An error occured! {error}"
+            )
+        if isinstance(error, commands.CommandOnCooldown):
+            return await ctx.send(
+                f"You are on cooldown!"
+            )
+        if isinstance(error, commands.CommandError):
+            return await ctx.send(
+                f"An error occured!"
+            )
